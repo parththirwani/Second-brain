b1a588d06bba258620010c0fadce37faf2b3f6e1
Parth Thirwani <parththirwaniw@gmail.com>
2025-12-22 20:44:10 +0530

    mongo

--- be/src/model/db.ts
+++ be/src/model/db.ts
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://parththirwaniw:ZFoN8zM4Pga3pKo1@cluster0.dplcn.mongodb.net/secondbrain");
+    await mongoose.connect(process.env.MONGO_URI || "");
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
     process.exit(1);
   }
 };
+
