c6e072f5031399d1d72c4220c7992624a5b68bae
Parth Thirwani <parththirwaniw@gmail.com>
2025-12-23 18:40:34 +0530

    types

---
+++ fe/src/types/types.ts
@@ -0,0 +1,31 @@
+export interface Document {
+  _id: string;
+  userId: string;
+  type: 'Document' | 'Tweet' | 'Youtube' | 'Link';
+  link: string;
+  title: string;
+  description?: string;
+  tags: string[];
+  sharable: boolean;
+  sharableId?: string;
+}
+
+export interface User {
+  token: string;
+}
+
+export interface Profile {
+  userId: string;
+  username: string;
+  profession?: string;
+  avatar?: string;
+  socialLinks?: {
+    XLink?: string;
+    InstagramLink?: string;
+    Whatsapp?: string;
+    MediumLink?: string;
+  };
+  bio?: string;
+  publicProfile: boolean;
+}
+
